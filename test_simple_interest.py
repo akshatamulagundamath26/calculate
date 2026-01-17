025789b4d343cad3d2ed88694bc8ed5772ee0cd4
akshata2006-eng <akshatamulagundamath26@gmail.com>
2026-01-17 13:00:09 +0530

    save

--- test_simple_interest.py
+++ test_simple_interest.py
@@ -2,12 +2,12 @@ from simple_interest import calculate_simple_interest
 
 
 def test_simple_interest_basic():
-    assert simple_interest(1000, 5, 2) == 100.0
+    assert calculate_simple_interest(1000, 10, 1) == 100.0
 
 
-def test_simple_interest_zero_time():
-    assert simple_interest(1500, 10, 0) == 0.0
+def test_simple_interest_zero_rate():
+    assert calculate_simple_interest(2000, 0, 5) == 0.0
 
 
 def test_simple_interest_decimal_values():
-    assert simple_interest(2500, 7.5, 3) == 562.5
+    assert calculate_simple_interest(1500, 7.5, 2) == 225.0
